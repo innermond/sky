f76d3c162c31545f31f64aca10b83f166d109627
mob <innermond@yahoo.com>
2017-05-10 15:56:56 +0300

    patch

--- http/person_handler.go
+++ http/person_handler.go
@@ -20,7 +20,7 @@ func NewPersonHandler(s sky.PersonService) *PersonHandler {
 	h.GET("/api/persons/:id", h.handleGetPerson)
 	h.DELETE("/api/persons/:id", h.handleDeletePerson)
 	h.POST("/api/persons", h.handlePostPerson)
-	h.PATCH("/api/persons", h.handlePatchPerson)
+	h.PATCH("/api/persons/:id", h.handlePatchPerson)
 
 	return h
 }
